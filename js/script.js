c8acb104bf2aadc1fdc74987ccb54d9b3b2d60c6
Vlad <example@gmail.com>
2022-03-13 21:52:53 +0300

    tabs changes

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContect = document.querySelectorAll('.tabcontect'),
           tabsParent = document.querySelector('.tabheader__items');
+          
 });
