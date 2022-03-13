1c3d6ee230a8175b52f0ed05013e1c90d182c208
Vlad <example@gmail.com>
2022-03-13 21:37:14 +0300

    tabs

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContect = document.querySelectorAll('.tabcontect'),
+          tabsParent = document.querySelector('.tabheader__items');
+});
