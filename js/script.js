6791d957e41430ab461f5e552f5c5010af9c34c3
Vlad <example@gmail.com>
2022-03-14 21:26:20 +0300

    hide-show tabs

--- js/script.js
+++ js/script.js
@@ -3,5 +3,22 @@ window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContect = document.querySelectorAll('.tabcontect'),
           tabsParent = document.querySelector('.tabheader__items');
-          
+
+    function hideTabContent() {
+        tabsContect.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i) {
+        tabsContect[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent(0);
 });
